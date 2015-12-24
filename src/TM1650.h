5478a5242ae809ae5b240ad514b65eb52780d371
Mario Mikočević <mozgy@t-com.hr>
2015-12-24 20:49:28 +0100

    Comforming library 1.5 format

--- src/TM1650.h
+++ src/TM1650.h
@@ -9,7 +9,7 @@
 #ifndef TM1650_H
 #define TM1650_H
 
-#define TM1650_VERSION			0.2
+#define TM1650_VERSION			1.0
 #define TM1650_DISPLAY_BASE		0x34	// Address of the left-most digit 
 #define TM1650_CONTROL_BASE		0x24	// Address of the control register of the left-most digit
 #define TM1650_NUM_DIGITS		4	// 4 segments
@@ -45,11 +45,27 @@ B7 B6 B5 B4 B3 B2 B1 B0 Explanation
 	0x40
 0x10		0x04
 	0x08
+			0x80
  */
 
 const char TM1650_Digit_Table[] = {
-//	   0     1     2     3     4     5     6     7     8     9
-	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F,	0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71
+	0x3F,	// 0
+	0x06,	// 1
+	0x5B,	// 2
+	0x4F,	// 3
+	0x66,	// 4
+	0x6D,	// 5
+	0x7D,	// 6
+	0x07,	// 7
+	0x7F,	// 8
+	0x6F,	// 9
+
+	0x77,
+	0x7C,
+	0x39,
+	0x5E,
+	0x79,
+	0x71
 };
 
 class TM1650 {
